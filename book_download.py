5f0628589d7a48ec93732ec8f7ceb3ee58188ecc
Ash2803 <ashot.93@mail.ru>
2022-12-28 20:05:08 +0400

    download books with titles

--- book_download.py
+++ book_download.py
@@ -1,19 +1,25 @@
-import requests
+import os
 from pathlib import Path
 
+import requests
+from pathvalidate import sanitize_filename
 
-def download_book():
+from bs4_tutorial import parse_post
+
+
+def download_txt(url):
     book_dir_path = Path('books')
     book_dir_path.mkdir(parents=True, exist_ok=True)
     for book_id in range(11):
         params = {
             'id': book_id
         }
-        response = requests.get('https://tululu.org/txt.php', params=params)
+        response = requests.get(url, params=params)
         response.raise_for_status()
         try:
             check_for_redirect(response)
-            with open(book_dir_path / f'Book_{book_id}.txt', 'wb') as file:
+            parsed_book_name = f'{sanitize_filename(parse_post(book_id))}.txt'
+            with open(book_dir_path / f'{book_id}. {parsed_book_name}', 'wb') as file:
                 file.write(response.content)
         except requests.HTTPError:
             pass
@@ -25,7 +31,7 @@ def check_for_redirect(response):
 
 
 def main():
-    download_book()
+    download_txt('https://tululu.org/txt.php')
 
 
 if __name__ == '__main__':
