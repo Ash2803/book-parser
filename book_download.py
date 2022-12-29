5480e45fdeb6439df205c81b7cf132b151a70e16
Ash2803 <ashot.93@mail.ru>
2022-12-29 05:25:23 +0400

    download comments.

--- book_download.py
+++ book_download.py
@@ -1,31 +1,45 @@
 import os
+import traceback
 from pathlib import Path
 
 import requests
 from pathvalidate import sanitize_filename
 import urllib.parse
-
 from parse import parse_book
 
 
-def download_book_image():
+def download_book_comments(url):
+    images_dir_path = Path('comments')
+    images_dir_path.mkdir(parents=True, exist_ok=True)
+    for book_id in range(4, 11):
+        try:
+            comments = parse_book(url, book_id)['comments']
+            parsed_book_name = f'{sanitize_filename(parse_book(url, book_id)["book_title"])}.txt'
+            with open(images_dir_path / f'{book_id}. {parsed_book_name}', 'w') as file:
+                for comment in comments:
+                    if comment:
+                        file.write(f'{comment}\n')
+        except requests.ConnectTimeout:
+            print(traceback.format_exc())
+
+
+def download_book_image(url):
     images_dir_path = Path('images')
     images_dir_path.mkdir(parents=True, exist_ok=True)
     for book_id in range(4, 11):
-        img_link = parse_book(book_id)['image_link']
+        img_link = parse_book(url, book_id)['image_link']
         split_url = urllib.parse.urlsplit(img_link)
         split_domain = urllib.parse.unquote(split_url[2])
         file_format = os.path.splitext(split_domain)
         if file_format[1] != '.gif':
-            response = requests.get(img_link)
-            response.raise_for_status()
             try:
-                check_for_redirect(response)
-                parsed_book_name = f'{sanitize_filename(parse_book(book_id)["book_title"])}.jpg'
+                response = requests.get(img_link)
+                response.raise_for_status()
+                parsed_book_name = f'{sanitize_filename(parse_book(url, book_id)["book_title"])}.jpg'
                 with open(images_dir_path / f'{book_id}. {parsed_book_name}', 'wb') as file:
                     file.write(response.content)
-            except requests.HTTPError:
-                pass
+            except requests.ConnectionError:
+                print(traceback.format_exc())
 
 
 def download_book_txt(url):
@@ -43,7 +57,7 @@ def download_book_txt(url):
             with open(book_dir_path / f'{book_id}. {parsed_book_name}', 'wb') as file:
                 file.write(response.content)
         except requests.HTTPError:
-            pass
+            print(traceback.format_exc())
 
 
 def check_for_redirect(response):
@@ -52,7 +66,8 @@ def check_for_redirect(response):
 
 
 def main():
-    download_book_image()
+    url = 'https://tululu.org'
+    download_book_comments(url)
 
 
 if __name__ == '__main__':
--- parse.py
+++ parse.py
@@ -4,12 +4,22 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_book(book_id) -> dict:
-    url = 'https://tululu.org'
+def parse_book(url, book_id) -> dict:
     response = requests.get(f'{url}/b{book_id}')
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     title = soup.find('div', id='content').find('h1')
     img = soup.find('table').find('div', class_='bookimage').find('img')['src']
     img_link = urljoin(url, img)
-    return {'image_link': img_link, 'book_title': title.text.split('::')[0].strip()}
+    comments = soup.find_all(class_='texts')
+    grouped_comments = []
+    for comment in comments:
+        grouped_comments.append(comment.find(class_='black').text)
+    return {'image_link': img_link,
+            'book_title': title.text.split('::')[0].strip(),
+            'comments': grouped_comments
+    }
+
+
+a = []
+print(a is True)
