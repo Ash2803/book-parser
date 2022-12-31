74f6674e1ff6cb3a23755b8abe166e5f89186843
Ash2803 <ashot.93@mail.ru>
2022-12-31 19:25:34 +0400

    Added ConnectionError and HTTPError handling

--- tululu.py
+++ tululu.py
@@ -1,4 +1,8 @@
 import argparse
+import logging
+import time
+
+import requests
 
 from book_download import download_book_txt, download_book_image, download_book_comments
 from parse import get_book_page, parse_book_page, check_for_redirect, RedirectedPage
@@ -11,22 +15,31 @@ def main():
     args = parser.parse_args()
     genre = 'Деловая литература'
     url = 'https://tululu.org'
+    logging.basicConfig(format="%(lineno)d %(funcName)s %(filename)s %(levelname)s %(message)s")
     for book_id in range(args.start_id, args.end_id):
         try:
-            book_page = get_book_page(url, book_id)
-            check_for_redirect(book_page)
-            parsed_book_page = parse_book_page(book_page)
-        except RedirectedPage:
-            continue
-        except TypeError:
-            continue
-        if genre in parsed_book_page['genres']:
-            download_book_comments(parsed_book_page, genre, book_id)
-            download_book_image(parsed_book_page, genre, book_id)
-            download_book_txt(parsed_book_page, genre, book_id)
-            print(f'Название: {parsed_book_page["book_title"]}\n'
-                  f'Автор: {parsed_book_page["author"]}\n'
-                  f'Жанр: {parsed_book_page["genres"]}')
+            time.sleep(5)
+            try:
+                book_page = get_book_page(url, book_id)
+                check_for_redirect(book_page)
+                parsed_book_page = parse_book_page(book_page)
+            except RedirectedPage:
+                logging.error('URL has been redirected')
+                continue
+            except TypeError:
+                logging.error('There is no available book to download')
+                continue
+            if genre in parsed_book_page['genres']:
+                download_book_comments(parsed_book_page, genre, book_id)
+                download_book_image(parsed_book_page, genre, book_id)
+                download_book_txt(parsed_book_page, genre, book_id)
+                print(f'Название: {parsed_book_page["book_title"]}\n'
+                      f'Автор: {parsed_book_page["author"]}\n'
+                      f'Жанр: {parsed_book_page["genres"]}')
+        except requests.exceptions.ConnectionError:
+            logging.error('Connection lost')
+        except requests.exceptions.HTTPError:
+            logging.error('Invalid url')
 
 
 if __name__ == '__main__':
